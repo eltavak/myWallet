ee0e44dcb3756f7c766c7c7e8a485b7e4878767c
eltavak <eltavak@gmail.com>
2025-12-20 00:22:48 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Это критично для корректной работы путей на GitHub Pages
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
   }
-});
+})
